1f165d435a1e7e8c3fa0168a5e9985f4404ccdf1
Jeremy Bass <jeremybass@cableone.net>
2014-11-19 01:11:43 -0800

    needs to be more forceful to correct the spine

--- Gruntfile.js
+++ Gruntfile.js
@@ -23,9 +23,25 @@ module.exports = function(grunt) {
 					privateKey: deployPrivateKey
 				}
 			}
+		},
+		bump: {
+			options: {
+				files: ['package.json'],
+				updateConfigs: [],
+				commit: true,
+				commitMessage: 'Release v%VERSION%',
+				commitFiles: ['package.json'], // '-a' for all files
+				createTag: true,
+				tagName: 'v%VERSION%',
+				tagMessage: 'Version %VERSION%',
+				push: true,
+				pushTo: 'upstream',
+				gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d' // options to use with '$ git describe'
+			}
 		}
 	});
 	
+	grunt.loadNpmTasks('grunt-bump');
 	grunt.loadNpmTasks('grunt-ssh');
 	grunt.registerTask('default', ['sshexec:test']);
 
